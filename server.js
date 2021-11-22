fffd7e20bd650de946265f25483b9ae6068a1f25
yurigarrido <y.garrido.yuri@gmail.com>
2021-11-22 12:57:10 -0300

    config express server

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const app = express()
+const { resolve } = require('path')
+
+  app.use('/', express.static(
+    resolve(
+      __dirname,
+      './build'
+    )
+  )
+)
+
+app.listen(process.env.PORT || 3000, (err) => {
+  if (err) { return console.log(err); }
+
+  console.log('tudo funcionando certinho');
+});
